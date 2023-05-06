39bd1833445dccc30cbf84b29d04e336e46f51b8
Ricardo <rscimc@gmail.com>
2023-05-06 18:08:42 -0300

    API Rest Java e Spring

---
+++ src/main/java/com/inicio/demo/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.inicio.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.inicio.demo.entities.Departament;
+
+@RestController
+@RequestMapping(value="/products")
+public class ProductController {
+    @GetMapping
+    public List<Product> getObjects() {
+        Departament d1 = new Departament(1L, "Tech");
+        Departament d2 = new Departament(2L, "Pet");
+       
+        Product p1 = new Product(1L, "Macbook Pro", 4000.00, d1);
+        Product p2 = new Product(2L, "PC Gamer", 5000.00, d1);
+        Product p3 = new Product(3L, "Pet House", 300.00, d2);
+
+        List <Product> list = Arrays.asList(p1, p2, p3);
+        
+        return list;
+    }
+    
+}
